b724a898762e8ae7862821329b1a0080869cc3de
Lucienpemberton <lucienpemberton@gmail.com>
2019-10-11 11:58:37 +0200

    Add files via upload

---
+++ galgje.py
@@ -0,0 +1,59 @@
+import time
+import random
+
+print ("Gemaakt door Lucien Kerssens en Siebe Roeleveld; 5V5")
+print ("Welkom bij galje")
+
+time.sleep(1)
+
+print("\nJe hebt in totaal 10 pogingen om het woord te raden.")
+time.sleep(1)
+
+words = ["geheim", "code", "python", "programmeren", "galgje"]
+
+word = words[random.randint(0, len(words) - 1)]
+
+guesses = ''
+
+turns = 10
+
+while turns > 0:         
+
+    failed = 0             
+
+    for char in word:      
+
+        if char in guesses:    
+    
+            print (char, end=" ")
+
+        else:
+    
+            print ("_", end=" ")
+       
+            failed += 1    
+
+    if failed == 0:        
+        print ("\nJe hebt het woord geraden!")  
+
+        break              
+
+
+    guess = input("\nTyp een letter...")
+    print("\n")
+
+    guesses += guess                    
+
+    if guess not in word:  
+ 
+        turns -= 1        
+ 
+    # print wrong
+        print ("\nFout!")
+ 
+        print ("Je hebt nog " + str(turns) + " pogingen")
+ 
+        if turns == 0:           
+    
+            print ("Je hebt het woord niet geraden :(")
+            print ("Het woord was: " + word)
